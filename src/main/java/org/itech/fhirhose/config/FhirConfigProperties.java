c6876dff127dc59ac736e50d1490cd553b40449a
CalebSLane <c.steelelane@gmail.com>
2021-07-01 16:33:58 -0700

    add missing component

--- src/main/java/org/itech/fhirhose/config/FhirConfigProperties.java
+++ src/main/java/org/itech/fhirhose/config/FhirConfigProperties.java
@@ -1,9 +1,11 @@
 package org.itech.fhirhose.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
+@Component
 @ConfigurationProperties(prefix = "org.itech.fhirhose.fhir")
 @Data
 public class FhirConfigProperties {
