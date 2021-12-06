57eaceca9837629b2b32f13482ae300a6555af1b
rossumg <rossumg@uw.edu>
2021-12-06 13:15:17 -0800

    move address and temp_address to questionnaire response

---
+++ src/main/java/org/itech/fhirhose/fhir/FhirConstants.java
@@ -0,0 +1,81 @@
+package org.itech.fhirhose.fhir;
+
+public class FhirConstants {
+
+    public static final String SEAT_LINK_ID = "17d1a007-799a-4038-801c-d0e487305585";
+
+    public static final String NATIONALITY_LINK_ID = "2af452e9-da1c-4ee8-8c86-f4a2156cd49b";
+
+    public static final String AIRLINE_LINK_ID = "c07051d4-d30d-4c15-b628-bd1e2c498395";
+
+    public static final String FLIGHT_LINK_ID = "f747a84b-abba-4326-8a08-03793e3e21b6";
+
+    public static final String COUNTRIES_VISTED_LINK_ID = "21451590-0483-43de-9ef9-2c268a0b0061";
+
+    public static final String PREVIOUS_INFECTION_LINK_ID = "49e45134-91ba-4f45-b388-ee7de62295ef";
+
+    public static final String FEVER_LINK_ID = "6dda8ad4-6388-40c0-ab67-af01aa9e6847";
+
+    public static final String SORE_THROAT_LINK_ID = "29f01800-994b-4f80-b357-d29752f1e91f";
+
+    public static final String JOINT_PAIN_LINK_ID = "e865684e-76e7-430a-af76-a26f1a2c6b9a";
+
+    public static final String COUGH_LINK_ID = "e5493420-d719-4c04-b14f-f4ffde0068db";
+
+    public static final String BREATHING_LINK_ID = "da899826-8191-4540-9d04-53a894e32ab6";
+
+    public static final String RASH_LINK_ID = "904c49d4-b520-4ef9-85d3-6468203c70a7";
+
+    public static final String SENSE_OF_SMELL_LINK_ID = "7262fddc-ba75-4056-8130-55cd26c6e76f";
+
+    public static final String CONTACT_WITH_NFECTED_LINK_ID = "10b4f559-e701-44b1-818c-f1321a120b72";
+
+    public static final String MOBILE_PHONE_LINK_ID = "1c4205a9-dcf9-41fc-b10c-904fd13ad070";
+
+    public static final String FIXED_PHONE_LINK_ID = "35911a46-ad58-47ee-b8ee-f4601c0a4875";
+
+    public static final String WORK_PHONE_LINK_ID = "7034d535-f1a3-4f14-8bab-12af53d7b67b";
+
+    public static final String EMAIL_LINK_ID = "4b5eeb23-c029-488c-b546-2293c834e4ef";
+
+    public static final String NATIONAL_ID_LINK_ID = "0394ae5a-7d75-4171-b3eb-22b434c7e29f";
+
+    public static final String PASSPORT_COUNTRY_LINK_ID = "be18527f-939d-4547-a157-fb3afcea9de3";
+
+    public static final String PASSPORT_NUMBER_LINK_ID = "b8e60de2-1933-468d-89ca-6ae46882d6d0";
+
+    public static final String DATE_OF_ARRIVAL_LINK_ID = "74eef3fb-f388-4d35-9349-b68aae0eb00b";
+
+    public static final String PURPOSE_OF_VIST_LINK_ID = "455e47e8-16d7-46ff-98fc-f9823d1cbf50";
+
+    public static final String HEALTH_OFFICE_LINK_ID = "be3de5e0-ad7b-4241-b1e9-84d7a17f900a";
+
+    public static final String TEST_KIT_ID_LINK_ID = "a844764b-afe5-4a8f-8219-4f0245e26e87";
+
+    public static final String PERM_ADDRESS_NUMBER_AND_STREET_LINK_ID = "6928894a-0b14-4358-a121-3186acfe3f83";
+
+    public static final String PERM_ADDRESS_APARTMENT_NUMBER_LINK_ID = "315277b9-0d20-4073-8e57-59889d7ce8ff";
+
+    public static final String PERM_ADDRESS_CITY_LINK_ID = "def4076e-32c6-409f-80a5-49357cf938b8";
+
+    public static final String PERM_ADDRESS_STATE_PROVINCE_LINK_ID = "f4e3f75a-2a21-4bb8-9fcd-cf8a0250e0a2";
+
+    public static final String PERM_ADDRESS_COUNTRY_LINK_ID = "5692cdd6-dbf2-4c45-ac1a-ea6635e4e439";
+
+    public static final String PERM_ADDRESS_ZIP_POSTAL_CODE_LINK_ID = "72bf20d2-c096-4bdb-b93c-1f98958af588";
+
+    public static final String TEMP_ADDRESS_HOTEL_NAME_LINK_ID = "d6e29219-9167-44d1-84db-797aa3fb9a7a";
+
+    public static final String TEMP_ADDRESS_NUMBER_AND_STREET_LINK_ID = "556cec94-35e6-4f86-8d49-cfc72ce45e3e";
+
+    public static final String TEMP_ADDRESS_APARTMENT_NUMBER_LINK_ID = "ec69bcc1-b718-4e6d-a1c1-db3812fc95ec";
+
+    public static final String TEMP_ADDRESS_CITY_LINK_ID = "c22fe2a4-dc61-439b-9058-8a10a0b6c909";
+
+    public static final String TEMP_ADDRESS_STATE_PROVINCE_LINK_ID = "09f3e16c-984e-4c2a-a9fc-24e2f31a44d3";
+
+    public static final String TEMP_ADDRESS_COUNTRY_LINK_ID = "8d8fb3af-96da-4405-8884-50553e0f54c7";
+
+    public static final String TEMP_ADDRESS_ZIP_POSTAL_CODE_LINK_ID = "03db31d6-a1cb-4199-9555-647649291894";
+
+}
